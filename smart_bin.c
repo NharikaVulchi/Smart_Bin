871a9712bfa9a704ee892b1ff70b9563e3af5e4d
NiharikaVulchi <83216569+NharikaVulchi@users.noreply.github.com>
2023-10-09 15:47:20 +0530

    Update smart_bin.c

--- smart_bin.c
+++ smart_bin.c
@@ -1,13 +1,42 @@
-void waste_detector();
-void start();
-
-#include<stdio.h>
 
 int ir; // output from IR sensor : 1 is for close proximity
 int metal_sen ; // output from the metal detecting sensor : 1 is for metal detected
 int mois_sen ; // output from the moisture detecting sensor : 1 is for moisture detected
 int mm,mw,md; // gpio's for motors operating the open-close of metal,wet and dry lids respectively
 int LED_value;
+
+void waste_detector();
+void start(int);
+
+int main(){
+    
+    ir=0; //ir=digital_read(ir_pin_number) 
+    asm volatile(
+    "addi %0 , x30, 0\n\t"
+    :"=r"(ir)
+    :
+    :
+    );
+    mois_sen=0; //mois_sen=digital_read(mois_sen_pin_number)
+    asm volatile(
+    "addi %0 , x30, 0\n\t"
+    :"=r"(mois_sen)
+    :
+    :
+    );
+    metal_sen =1; //metal_sen=digital_read(metal_sen_pin_number)
+    asm volatile(
+    "addi %0 , x30, 4\n\t"
+    :"=r"(metal_sen)
+    :
+    :
+    );
+    while(1){
+        start(ir);
+    }
+    return(0);
+}
+
 
 void waste_detector(int mois_sen,int metal_sen,int mm,int mw,int md)
 {
@@ -29,7 +58,7 @@ void start(int ir)
     
 	if(!ir) //If the bin is not full, we manage the waste segregation
 	{
-		printf("Bin is full\n");
+		//printf("Bin is full\n");
 		LED_value=0; //digital_write(LED_pin_number,0)
 		int led_value_2 = LED_value * 16;
 		asm volatile(
@@ -81,31 +110,4 @@ void start(int ir)
 	
 	
 }
-int main(){
-    
-    ir=0; //ir=digital_read(ir_pin_number) 
-    asm volatile(
-    "addi %0 , x30, 0\n\t"
-    :"=r"(ir)
-    :
-    :
-    );
-    mois_sen=0; //mois_sen=digital_read(mois_sen_pin_number)
-    asm volatile(
-    "addi %0 , x30, 0\n\t"
-    :"=r"(mois_sen)
-    :
-    :
-    );
-    metal_sen =1; //metal_sen=digital_read(metal_sen_pin_number)
-    asm volatile(
-    "addi %0 , x30, 4\n\t"
-    :"=r"(metal_sen)
-    :
-    :
-    );
-    while(1){
-        start(ir);
-    }
-    return(0);
-}
+
